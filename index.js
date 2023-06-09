f7a5a2c36bf3a03397fb1f5ddf10ef7bcdc78658
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-06-09 15:10:22 +0600

    make admin function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -36,12 +36,24 @@ async function run() {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
             res.send(result)
+        });
+
+        app.patch('users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result)
         })
 
         app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result)
-        })
+        });
         //user collection
 
         // class collection
